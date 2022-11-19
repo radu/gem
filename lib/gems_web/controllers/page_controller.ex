60511703c80abbd6505ad1e229b7532421c14167
radu <radu@raduta.net>
2022-11-19 01:26:09 -0800

    debug

--- lib/gems_web/controllers/page_controller.ex
+++ lib/gems_web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule GEMSWeb.PageController do
   use GEMSWeb, :controller
 
   def index(conn, _params) do
+    Logger.debug("PageController #{inspect(self())} " <> conn )
     render(conn, "index.html")
   end
 end
